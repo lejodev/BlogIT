ee41be4e7a92619afe204b97e64f239a54a40992
lejodev <alejandro.rv97@gmail.com>
2024-06-06 10:00:28 -0500

    Loading page edited

--- components/Loaading.jsx
+++ components/Loaading.jsx
@@ -13,8 +13,11 @@ const Loaading = () => {
           <span className="visually-hidden">Loading...</span>
         </div>
         <h1 className="mt-3">LOADING</h1>
-        <p className="lead">
-          Waiting for server to get the blogs. This can take up to 50 seconds.
+        <p className="d-flex flex-column lead">
+          <span>
+            Waiting for server to get the blogs. This can take up to 50 seconds.
+          </span>
+          <span> Its a free Tier. You kow how it is! </span>
         </p>
       </div>
     </Container>
